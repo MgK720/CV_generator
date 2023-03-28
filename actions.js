7ce52b6fb4619e2d9c3353496279f5b862500c24
MgK720 <igorhg720@gmail.com>
2023-03-28 18:50:59 +0200

    js for education section

--- actions.js
+++ actions.js
@@ -1,3 +1,10 @@
+function setAttributes(attrib, values){
+    for(var key in values){
+        attrib.setAttribute(key,values[key]);
+    }
+}
+
+
 function schoolTypeShow() {
     var knowledge = document.getElementById("knowledge-type").value;
     if (knowledge == "school") {
@@ -7,6 +14,12 @@ function schoolTypeShow() {
         document.getElementById("school-type-select").style.display = "none";
     }
 };
+function createSpanValidity(){
+    var spanValidity = document.createElement("span");
+    spanValidity.setAttribute("class","validity");
+    return spanValidity;
+}
+
 var addEducationCount = 0;
 //Naprawic zeby dzialalo zawsze
 window.onload = document.getElementById("addEducation").addEventListener("click", function() {
@@ -18,12 +31,24 @@ window.onload = document.getElementById("addEducation").addEventListener("click"
 
     var newSection = document.createElement("section")
     newSection.setAttribute("id", "education" + addEducationCount);
+    newSection.setAttribute("class", "education");
 
     //Tutaj musi sie zrobic caly form education
-    var knowledge = document.createElement("INPUT");
-    knowledge.setAttribute("type", "text");
+    var knowledgeLabel = document.createElement("label");
+    setAttributes(knowledgeLabel, {"for": "school" + addEducationCount})
+    knowledgeLabel.textContent = "Knowledge:";
 
+    var knowledge = document.createElement("INPUT");
+    setAttributes(knowledge, {"type": "text", "name": "school" + addEducationCount, "id" : "school" + addEducationCount, 
+    "class" : "school", "minlength" : "1", "maxlength" : "45", "required": "true"});
+
+    var spanValidity = createSpanValidity();
+
+    
+
+    newSection.appendChild(knowledgeLabel);
     newSection.appendChild(knowledge);
+    newSection.appendChild(spanValidity);
 
 
     //document.getElementById("myform").appendChild(newSection);
