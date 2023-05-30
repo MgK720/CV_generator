65e43adbadb8ca79c6c1e96144760878c6945ff6
MgK720 <igorhg720@gmail.com>
2023-05-30 20:38:52 +0200

    process.env variables in get.js

--- server/get.js
+++ server/get.js
@@ -1,10 +1,12 @@
 const Pool = require('pg').Pool
+require('dotenv').config();
+
 const pool = new Pool({
-    user: 'me',
-    host: 'localhost',
-    database: 'cvgen_test',
-    password: 'password',
-    port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 
 const getCv = async (request, response) => {
