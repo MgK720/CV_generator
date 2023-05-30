d5ec545d92bb29aa33743720ed3ed33bc786a977
MgK720 <igorhg720@gmail.com>
2023-05-30 20:44:29 +0200

    get.js changes

--- server/get.js
+++ server/get.js
@@ -11,35 +11,31 @@ const pool = new Pool({
 
 const getCv = async (request, response) => {
     const id = request.params.id;
-    outputData = {
-        personaldata: [],
-        knowledge: [],
-        experience: [],
-        skill: [],
-        hobby: [],
-        link: []
-    }
 
     try{
         const getPersonalData = await pool.query('SELECT * FROM personaldata WHERE cv_id = $1', [id]);
-        outputData.personaldata = getPersonalData.rows;
 
         const getKnowledgeData = await pool.query('SELECT * FROM knowledge WHERE cv_id = $1', [id]);
-        outputData.knowledge = getKnowledgeData.rows;
 
         const getExperienceData = await pool.query('SELECT * FROM job WHERE cv_id = $1', [id]);
-        outputData.experience = getExperienceData.rows;
 
         const getSkillData = await pool.query('SELECT * FROM skill WHERE cv_id = $1', [id]);
-        outputData.skill = getSkillData.rows;
 
         const getHobbyData = await pool.query('SELECT * FROM hobby WHERE cv_id = $1', [id]);
-        outputData.hobby = getHobbyData.rows;
 
         const getLinkData = await pool.query('SELECT * FROM link WHERE cv_id = $1', [id]);
-        outputData.link = getLinkData.rows;
+
+        const outputData = {
+            personaldata: getPersonalData.rows,
+            knowledge: getKnowledgeData.rows,
+            experience: getExperienceData.rows,
+            skill: getSkillData.rows,
+            hobby: getHobbyData.rows,
+            link: getLinkData.rows
+        };
 
         console.log(outputData);
+        //render output or fetch in client side js
         response.status(200).send(outputData);
     }catch (error){
         console.log(error);
