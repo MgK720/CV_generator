6023a7c521341d2e8e7707295e30e98723b922ba
MgK720 <igorhg720@gmail.com>
2023-06-01 20:49:37 +0200

    .

--- api/create.js
+++ api/create.js
@@ -1,5 +1,6 @@
 const {upload, getFileDetails} = require('./upload_img.js')
 const Pool = require('pg').Pool
+require('dotenv').config();
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
